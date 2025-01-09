c35d8ad7d0eb49be8dfbaa71bead1dded6836a80
Alansno <techoro1230@gmail.com>
2025-01-09 15:21:19 -0600

    [ADD]Dapper context, globalFilters, and more

--- Infrastructure/InfrastructureDependencyInjection.cs
+++ Infrastructure/InfrastructureDependencyInjection.cs
@@ -7,13 +7,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Context;
 
 namespace Infrastructure
 {
     public static class InfrastructureDependencyInjection
     {
         public static IServiceCollection AddRepositories(this IServiceCollection services)
-            => services.AddScoped<IRepository<UserEntity>, UserRepository>();
+            => services.AddScoped<IRepository<UserEntity>, UserRepository>().AddDapperContext();
 
+        private static IServiceCollection AddDapperContext(this IServiceCollection services)
+            => services.AddScoped<DapperContext>();
     }
 }
